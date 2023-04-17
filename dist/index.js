21e68febd47f785e11b8d4ca59e695fc3366f4d4
Daniel09s <daniel0842s@gmail.com>
2023-04-16 22:02:35 -0500

    Creacion y modificacion de archivos index.html y index.ts para validar datos de usuario

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Hola mundo");
+const btnGuardar = document.getElementById('btn-guardar'); // Tratar como un elemento html
+btnGuardar.addEventListener('click', (e) => {
+    e.preventDefault(); // Evitar que se recargue la pagina
+    console.log('Ingreso a la validacion del formulario');
+    //Validacion formulario
+    //obtener datos de los inputs
+    const tipoDocumento = document.getElementById('tipoDocumento');
+    const numeroDocumento = document.getElementById('numeroDocumento');
+    const nombre = document.getElementById('nombre');
+    const apellido = document.getElementById('apellido');
+    const numeroCelular = document.getElementById('numeroCelular');
+    const email = document.getElementById('email');
+    const usuarioLink = document.getElementById('usuarioLi');
+    const usuarioGit = document.getElementById('usuarioGi');
+    console.log(tipoDocumento.value);
+});
